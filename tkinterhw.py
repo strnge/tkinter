33036573c8f3fa234c647f588972fef049c4aea2
max <max.wi.strange@gmail.com>
2025-04-11 15:39:36 -0400

    comments

--- tkinterhw.py
+++ tkinterhw.py
@@ -11,31 +11,34 @@ class Tkinter_Test:
         root.columnconfigure(0, weight=1)
         frm = ttk.Frame(root, borderwidth=2, relief="sunken", width=240, height=240, padding=10)#creates a frame widget that is fit inside the root window
         frm.grid(rowspan=7, columnspan=2, sticky="nsew")#initializes a relative grid for the window within the frame
-        frm.columnconfigure((0,1), weight=1)
+        frm.columnconfigure((0,1), weight=1)#used to control resize behavior of columns and rows, can either be used on a per row/col basis or on a list of rows/columns
         frm.rowconfigure((0,1,2,3,4,5,6), weight=1)
-        self.ulabel = tk.StringVar(value="Label")
+
+        self.ulabel = tk.StringVar(value="Label")#these tk stringvars are bound to python vars, and when updated are automatically reflected in the ui
         self.ulabel_display = tk.StringVar(value="Label")
 
         ttk.Label(frm, textvariable=self.ulabel_display).grid(column=0, row=0,)#inserts a static label , using columns and rows for relative positioning within the previously created grid
-        ttk.Entry(frm, textvariable=self.ulabel).grid(column=0, row=1)
+        ttk.Entry(frm, textvariable=self.ulabel).grid(column=0, row=1)#input field for user text
         ttk.Button(frm, text="Submit Label",command=self.submit_label).grid(column=0, row=2)#creates a submit button that will change the label above it directly below
         
         ttk.Label(frm, text="Window Title").grid(column=1, row=0)
-        ttk.Entry(frm, textvariable=self.utitle).grid(column=1, row=1)
+        ttk.Entry(frm, textvariable=self.utitle).grid(column=1, row=1)#input field for user text
         ttk.Button(frm, text="Submit Title",command=self.submit_title).grid(column=1, row=2)#creates a submit button that will change the window title
         
-        rightsideFrm = ttk.Frame(root, borderwidth=2, relief="groove", padding=5, width=80, height=240)
-        rightsideFrm.grid(columnspan=2, column=2, rowspan=7, row=0, sticky="nsew")
+        rightsideFrm = ttk.Frame(root, borderwidth=2, relief="groove", padding=5, width=80, height=240)#generates subframe for rightside
+        rightsideFrm.grid(columnspan=2, column=2, rowspan=7, row=0, sticky="nsew")#sets up grid for placement control
        
         self.win_width = tk.StringVar(value="0")
         self.win_height = tk.StringVar(value="0")
-        root.bind("<Configure>",self.on_resize)
+        root.bind("<Configure>",self.on_resize)#binds the <Configure> event to callback the on_resize function, which will update the size display
 
+        #create size display widgets
         ttk.Label(rightsideFrm, text="Width(px)").grid(column=0,row=1,rowspan=3)
         ttk.Label(rightsideFrm, textvariable=self.win_width).grid(column=1,row=1,rowspan=3,ipadx=3)
         ttk.Label(rightsideFrm, text="Height(px)").grid(column=0,row=4,rowspan=3)
         ttk.Label(rightsideFrm, textvariable=self.win_height).grid(column=1,row=4,rowspan=3,ipadx=3)
 
+        #bottom span widget that contains 4x checkboxes
         bottomFrm = ttk.Frame(root, borderwidth=2, relief="groove", padding=5)
         bottomFrm.grid(columnspan=4, column=0, row=7, sticky="we")
         bottomFrm.columnconfigure((0,1,2,3), weight=1)
@@ -44,6 +47,7 @@ class Tkinter_Test:
         ttk.Checkbutton(bottomFrm, text="Check 2").grid(column=1, row=7)
         ttk.Checkbutton(bottomFrm, text="Check 3").grid(column=2, row=7)
         ttk.Checkbutton(bottomFrm, text="Check 4").grid(column=3, row=7)
+        
         ttk.Button(frm, text="Quit",command=root.destroy).grid(column=0, row=6, columnspan=2)#creates a Quit button
     
     def on_resize(self, event):
